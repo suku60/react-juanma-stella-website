67d0382a843ee23fcd799dc44eb0f2dc9069dff0
Juan Manuel Stella <0x86jm14sMb94@gmail.com>
2022-06-28 22:03:40 +0200

    Small changed on Contact section.

--- src/Components/SectionContact/SectionContact.jsx
+++ src/Components/SectionContact/SectionContact.jsx
@@ -5,10 +5,10 @@ const SectionContact = () => {
 
 return (
   <div className="size_main_section main_section_contact">
-            <div className="contact_title centered_content">
-              <p>contact</p>
-            </div>
-            <div className="contact_data"></div>
+    <div className="contact_title centered_content">
+      <p>contact</p>
+    </div>
+    <div className="contact_data"></div>
             
   </div>
   )
